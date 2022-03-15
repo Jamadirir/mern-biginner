0a06bfcc5371640c7a3123b771d5250def3a9f2f
jamadirir <jamafarah2522@gmail.com>
2022-03-15 15:24:52 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const FriendModel = require("./models/Friends");
+const cors=require('cors')
+require('dotenv').config()
+app.use(cors())
+app.use(express.json())
+/// DATABASE CONNECTION
+mongoose.connect(
+  "mongodb://localhost:27017/tutorialmern?readPreference=primary&appname=MongoDB%20Compass&ssl=false",
+  { useNewUrlParser: true }
+);
+
+app.post("/addfriend", async (req, res) => {
+  const name=req.body.name
+  const age=req.body.age
+  const friend = new FriendModel({ name:name, age: age });
+  await friend.save();
+  res.send(friend);
+});
+
+app.get("/read", async (req, res) => {
+  FriendModel.find({}, (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.put('/update', async(req,res)=>{
+  const newAge=req.body.newAge
+  const id=req.body.id
+  try {
+    await FriendModel.findById(id,(error,updateToFriend)=>{
+      updateToFriend.age=Number(newAge)
+      updateToFriend.save()
+    })
+  } catch (error) {
+    console.log(error)
+  }
+  res.send('updated')
+})
+
+
+app.delete('/delete/:id',async(req,res)=>{
+  const id=req.params.id
+  await FriendModel.findByIdAndRemove(id).exec(
+    res.send('deleted')
+  ) 
+})
+app.listen(process.env.PORT || 5000, () => {
+  console.log("You are connected!");
+});
